da5a4c6dc420554b4c7454b605a01726271847ca
ZIZUHENSEM <89843873+ZIZUHENSEM@users.noreply.github.com>
2021-10-08 18:18:31 +0700

    Update help.js

--- message/help.js
+++ message/help.js
@@ -826,4 +826,4 @@ exports.allMenu = (prefix, cmhit, runtime, pushname, isOwner, isPremium, gcount,
 *▢ ${prefix}memeindo*
 *▢ ${prefix}darkjoke*
 
-©𝑪𝒓𝒆𝒂𝒕𝒐𝒓 𝒃𝒚 𝒚𝒖𝒅𝒉𝒂 𝒑𝒆𝒓𝒅𝒂𝒏𝒂`}
+©𝑪𝒓𝒆𝒂𝒕𝒐𝒓 𝒃𝒚 zizu ganz`}
